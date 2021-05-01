441065033f9714ea5942a1a34cb131de5cb42e09
nikistefan <nikistefan95@gmail.com>
2021-05-01 23:04:46 +0300

    maked script for player movement

---
+++ Assets/scripts/MovePlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MovePlayer : NetworkBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float speed = 4f;
+
+    public void movement()
+    {
+        if (!isLocalPlayer) return;
+        float yMov = Input.GetAxisRaw("Vertical");
+        transform.Translate(0, yMov * speed * Time.deltaTime, 0);
+    }
+
+    private void Start()
+    {
+        rb.gravityScale = 0;
+    }
+
+    private void Update()
+    {
+        movement();
+    }
+}
