88c510a550f2dce9def316d43ff98fdc612adffd
nikistefan <nikistefan95@gmail.com>
2021-05-03 00:11:26 +0300

    maked scroe system

--- Assets/scripts/PereteInvisibil.cs
+++ Assets/scripts/PereteInvisibil.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class PereteInvisibil : MonoBehaviour
 {
     public static bool player1Dies = false;
-    public static int player1DiesCount = 0;//statcic
+    [SerializeField]
+    //public static int player1DiesCount = 0;//statcic
+    public static int player1Lives = 5;
 
     public static bool player2Dies = false;
-    public static int player2DiesCount = 0;//statcic
+    //public static int player2DiesCount = 0;//statcic
+    [SerializeField]
+    public static int player2Lives = 5;
 
     public void Player2Die()
     {
         player2Dies = true;
-        ++player2DiesCount;
+        //++player2DiesCount;
+        --player2Lives;
     }
 
     public void Player1Die()
     {
         player1Dies = true;
-        ++player1DiesCount;
+        //++player1DiesCount;+--player1DiesCount;
+        --player1Lives;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -27,12 +33,12 @@ public class PereteInvisibil : MonoBehaviour
         if (collision.gameObject.tag == "PereteInvisibilPlayer1")
         {
             Player1Die();
-            Debug.LogWarning(player1Dies + " " + player1DiesCount);
+            //Debug.LogWarning(player1Dies + " " + player1DiesCount);
         }
         if(collision.gameObject.tag == "PereteInvisibilPlayer2")
         {
             Player2Die();
-            Debug.LogWarning(player2Dies + " " + player2DiesCount);
+            //Debug.LogWarning(player2Dies + " " + player2DiesCount);
         }
     }
 }
--- Assets/scripts/ScoreSystem.cs
+++ Assets/scripts/ScoreSystem.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using static PereteInvisibil;
 
-public class ScoreSystem : MonoBehaviour
+public class ScoreSystem : MonoBehaviour//scrore
 {
-    // Start is called before the first frame update
-    void Start()
+    public PereteInvisibil perete;
+
+    public TMP_Text score;
+
+    public string Scor;
+
+    //public int player1Dies;
+    //public int player2Dies;
+
+    public int player1Lives;
+    public int player2Lives;
+
+    public int StartScore;
+
+    public void WriteScore()
     {
-        
+        //player1Dies = PereteInvisibil.player1DiesCount;
+        //player2Dies = PereteInvisibil.player2DiesCount;
+
+        player1Lives = PereteInvisibil.player1Lives;
+        player2Lives = PereteInvisibil.player2Lives;
+
+        Scor = player1Lives + " : " + player2Lives;
+        score.text = Scor;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        WriteScore();
     }
 }
